cacd37113338dbc3abc18c6900a2a4a624568b5d
Kevin Luis Linuhung <kevinlinuhung@gmail.com>
2021-06-23 20:11:43 +0700

    fix text view warning

--- app/src/main/java/com/ventustium/mqtt/BikeActivity1.kt
+++ app/src/main/java/com/ventustium/mqtt/BikeActivity1.kt
@@ -7,9 +7,9 @@ import org.eclipse.paho.client.mqttv3.MqttMessage
 
 class BikeActivity1 : AppCompatActivity() {
 
-    lateinit var broker: String
+    private lateinit var broker: String
 
-    val mqttClient: MqttClient by lazy {
+    private val mqttClient: MqttClient by lazy {
         MqttClient(this)
     }
 
@@ -49,73 +49,87 @@ class BikeActivity1 : AppCompatActivity() {
         when (topic) {
             "/driver/heartrate" -> {
                 findViewById<TextView>(R.id.textViewDriver).apply {
-                    text = String(message.payload) + " BPM"
+                    val msg = String(message.payload) + " BPM"
+                    text = msg
                 }
             }
             "/driver/temperature" -> {
                 findViewById<TextView>(R.id.textViewDriverTemp).apply {
-                    text = String(message.payload) + "°C"
+                    val msg = String(message.payload) + "°C"
+                    text = msg
                 }
             }
             "/driver/spo2" -> {
                 findViewById<TextView>(R.id.textViewDriver5).apply {
-                    text = String(message.payload) + "%"
+                    val msg = String(message.payload) + "%"
+                    text = msg
                 }
             }
             "/driver/status" -> {
                 findViewById<TextView>(R.id.textViewDriver6).apply {
-                    text = String(message.payload)
+                    val msg = String(message.payload)
+                    text = msg
                 }
             }
 
             "/bike/status" -> {
                 findViewById<TextView>(R.id.textViewBikeStatus).apply {
-                    text = String(message.payload)
+                    val msg = String(message.payload)
+                    text = msg
                 }
             }
             "/bike/fwheel" -> {
                 findViewById<TextView>(R.id.textViewBikeFW).apply {
-                    text = String(message.payload) + "psi"
+                    val msg = String(message.payload) + "psi"
+                    text = msg
                 }
             }
             "/bike/bwheel" -> {
                 findViewById<TextView>(R.id.textViewBikeBW).apply {
-                    text = String(message.payload) + "psi"
+                    val msg = String(message.payload) + "psi"
+                    text = msg
                 }
             }
             "/bike/battery" -> {
                 findViewById<TextView>(R.id.textViewBikeBattery).apply {
-                    text = String(message.payload) + "Wh"
+                    val msg = String(message.payload) + "Wh"
+                    text = msg
                 }
             }
             "/bike/voltage" -> {
                 findViewById<TextView>(R.id.textViewBikeVoltage).apply {
-                    text = String(message.payload) + "V"
+                    val msg = String(message.payload) + "V"
+                    text = msg
                 }
             }
             "/bike/amps" -> {
                 findViewById<TextView>(R.id.textViewBikeCurrent).apply {
-                    text = String(message.payload) + "A"
+                    val msg = String(message.payload) + "A"
+                    text = msg
                 }
             }
             "/bike/power" -> {
                 findViewById<TextView>(R.id.textViewBikePower).apply {
-                    text = String(message.payload) + "W"
+                    val msg = String(message.payload) + "W"
+                    text = msg
                 }
             }
             "/bike/speed" -> {
                 findViewById<TextView>(R.id.textViewBikeSpeed).apply {
-                    text = String(message.payload) + "KM/H"
+                    val msg = String(message.payload) + "KM/H"
+                    text = msg
                 }
             }
             "/bike/range" -> {
                 findViewById<TextView>(R.id.textViewBikeRange).apply {
-                    text = String(message.payload) +"KM"
+                    val msg = String(message.payload) +"KM"
+                    text = msg
                 }
             }
             "/bike/estimation" -> {
                 findViewById<TextView>(R.id.textViewEstimation).apply {
-                    text = String(message.payload) + "KM"
+                    val msg = String(message.payload) + "KM"
+                    text = msg
                 }
             }
         }
